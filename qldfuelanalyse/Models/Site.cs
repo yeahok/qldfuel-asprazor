e6bda1f8871a99b7263bb216b5be2c63de552bd7
yeahok <yeahok@users.noreply.github.com>
2020-03-30 11:16:06 +1000

    Initial code commit

---
+++ qldfuelanalyse/Models/Site.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace qldfuelanalyse.Models
+{
+    public class Site
+    {
+        public int SiteId { get; set; }
+        public string SiteName { get; set; }
+        public string SiteBrand { get; set; }
+        public string SitesAddressLine1 { get; set; }
+        public string SiteSuburb { get; set; }
+        public string SiteState { get; set; }
+        public int? SitePostCode { get; set; }
+        public decimal? SiteLatitude { get; set; }
+        public decimal? SiteLongitude { get; set; }
+
+    }
+}
---
+++ qldfuelanalyse/Pages/Sites/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using qldfuelanalyse.Models;
+
+namespace qldfuelanalyse.Pages.Sites
+{
+    public class IndexModel : PageModel
+    {
+        static HttpClient client = new HttpClient();
+        public List<Site> sites { get; set; }
+
+        public async Task OnGet()
+        {
+            HttpResponseMessage response = await client.GetAsync(
+                "https://localhost:44338/api/sites");
+            sites = JsonConvert.DeserializeObject<List<Site>>(
+                await response.Content.ReadAsStringAsync());
+        }
+    }
+}
