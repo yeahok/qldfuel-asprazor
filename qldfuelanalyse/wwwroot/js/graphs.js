8038e48bbcd2ad456ff2188d4fd296a2259889fd
yeahok <yeahok@users.noreply.github.com>
2020-04-02 09:07:50 +1000

    Add price graph to details page

---
+++ qldfuelanalyse/wwwroot/js/graphs.js
@@ -0,0 +1,34 @@
+﻿function create_single_price_graph(url, graphTitle, divId) {
+    fetch(url)
+    .then(
+        response => response.json()
+    ).then(function (data) {
+        var vlSpec = {
+            $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+            title: graphTitle,
+            height: 500,
+            width: 'container',
+            data: {
+                values: data.map(function (element) { element['price'] /= 1000; return element })
+            },
+            mark: {
+                type: 'point',
+                clip: 'true'
+            },
+            encoding: {
+                y: {
+                    field: 'price',
+                    type: 'quantitative',
+                    axis: { title: 'Price' },
+                    scale: { domain: [1, 2] }
+                },
+                x: {
+                    field: 'transactionDateutc',
+                    type: "temporal",
+                    axis: { title: 'Date' }
+                }
+            }
+        };
+        vegaEmbed("#"+divId, vlSpec);
+    });
+}
