e663c661724e42dd29b8f29ecb80eb5d1e69d9b1
yeahok <yeahok@users.noreply.github.com>
2020-04-05 10:16:44 +1000

    Add graph to stats page

--- qldfuelanalyse/Pages/Stats.cshtml.cs
+++ qldfuelanalyse/Pages/Stats.cshtml.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace qldfuelanalyse.Pages
 {
     public class StatsModel : PageModel
     {
+        public string FuelApiBaseUrl { get; set; }
+        public StatsModel(IConfiguration configuration)
+        {
+            FuelApiBaseUrl = configuration.GetConnectionString("qldfuel-aspwebapiUrl");
+        }
+
         public void OnGet()
         {
 
---
+++ qldfuelanalyse/wwwroot/js/statspage.js
@@ -0,0 +1,37 @@
+﻿function create_all_price_graph(baseUrl, fuelType, divId) {
+    let url = `${baseUrl}?fueltype=${fuelType}`;
+    let graphTitle = `${fuelType} Price vs Time (All Sites)`;
+
+    fetch(url)
+        .then(
+            response => response.json()
+        ).then(function (data) {
+            var vlSpec = {
+                $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+                title: graphTitle,
+                height: 500,
+                width: 'container',
+                data: {
+                    values: data.map(function (element) { element['price'] /= 1000; return element })
+                },
+                mark: {
+                    type: 'point',
+                    clip: 'true'
+                },
+                encoding: {
+                    y: {
+                        field: 'price',
+                        type: 'quantitative',
+                        axis: { title: 'Price' },
+                        scale: { domain: [1,2] }
+                    },
+                    x: {
+                        field: 'transactionDateutc',
+                        type: "temporal",
+                        axis: { title: 'Date' }
+                    }
+                }
+            };
+            vegaEmbed("#" + divId, vlSpec);
+        });
+}
