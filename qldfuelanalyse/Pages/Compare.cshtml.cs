227205926c6babc5fb0a4b3ad902a43d8a08d0e4
yeahok <yeahok@users.noreply.github.com>
2020-04-06 11:12:15 +1000

    Added more placeholder stuff to compare page

--- qldfuelanalyse/Pages/Compare.cshtml.cs
+++ qldfuelanalyse/Pages/Compare.cshtml.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace qldfuelanalyse.Pages
 {
     public class CompareModel : PageModel
     {
+        public string FuelApiBaseUrlClient { get; set; }
+        public CompareModel(IConfiguration configuration)
+        {
+            FuelApiBaseUrlClient = configuration.GetConnectionString("qldfuel-aspUrl-client");
+        }
         public void OnGet()
         {
 
---
+++ qldfuelanalyse/wwwroot/js/comparepage.js
@@ -0,0 +1,52 @@
+﻿function create_multi_price_graph(baseUrl, siteId1, siteId2, fuelType, divId) {
+    let urls = [];
+    urls[0] = `${baseUrl}${siteId1}?fueltype=${fuelType}`;
+    urls[1] = `${baseUrl}${siteId2}?fueltype=${fuelType}`;
+    let graphTitle = `${fuelType} Price vs Time`;
+
+    Promise.all(urls.map(url =>
+        fetch(url)))
+        .then(function (response) {
+            var jsonData = [];
+            for (var i = response.length - 1; i >= 0; i--) {
+                jsonData.push(response[i].json());
+            }
+            return Promise.all(jsonData);
+        }
+    ).then(function (data) {
+        //var ok = data.map(function (element) { element['price'] /= 1000; return element })
+        var flattenedArray = data[0].concat(data[1]);
+        console.log(flattenedArray);
+            var vlSpec = {
+                $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+                title: graphTitle,
+                height: 500,
+                width: 'container',
+                data: {
+                    values: flattenedArray
+                },
+                mark: {
+                    type: 'line',
+                    clip: 'true'
+                },
+                encoding: {
+                    y: {
+                        field: 'price',
+                        type: 'quantitative',
+                        axis: { title: 'Price' }
+                    },
+                    x: {
+                        field: 'transactionDateutc',
+                        type: "temporal",
+                        axis: { title: 'Date' }
+                    },
+                    color: {
+                        field: 'siteId',
+                        type: 'nominal'
+                    }
+                }
+            };
+            vegaEmbed("#" + divId, vlSpec);
+        });
+}
+
