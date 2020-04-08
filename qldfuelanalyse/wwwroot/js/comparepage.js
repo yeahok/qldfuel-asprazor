f07afcfd1eec7ea69fba8b7f1784a1606b7e898e
yeahok <yeahok@users.noreply.github.com>
2020-04-08 20:49:46 +1000

    Move values to hidden fields on comparison page to retain data after refresh

--- qldfuelanalyse/wwwroot/js/comparepage.js
+++ qldfuelanalyse/wwwroot/js/comparepage.js
@@ -1,17 +1,17 @@
 ﻿//create comparison graph using information from 'inputClass' fields
-function create_multi_price_graph(baseUrl, inputClass, fuelType, divId) {
-    var idFields = document.getElementsByClassName("inputSiteId tt-input");
+function create_multi_price_graph(baseUrl, inputNameClass, inputIdClass, fuelType, divId) {
+    var idFields = document.getElementsByClassName("inputSiteId");
+    var nameFields = document.getElementsByClassName("inputSiteName");
 
     let urls = [];
 
-    urls[0] = `${baseUrl}${idFields[0].dataset.siteId}?fueltype=${fuelType}`;
-    urls[1] = `${baseUrl}${idFields[1].dataset.siteId}?fueltype=${fuelType}`;
+    urls[0] = `${baseUrl}${idFields[0].value}?fueltype=${fuelType}`;
+    urls[1] = `${baseUrl}${idFields[1].value}?fueltype=${fuelType}`;
 
     let titles = [];
 
-    titles[0] = idFields[0].dataset.siteName;
-    titles[1] = idFields[1].dataset.siteName;
-    console.log(titles);
+    titles[0] = nameFields[0].value;
+    titles[1] = nameFields[1].value;
 
     let graphTitle = `${fuelType} Price vs Time`;
     
@@ -27,9 +27,10 @@ function create_multi_price_graph(baseUrl, inputClass, fuelType, divId) {
     ).then(function (data) {
         var mergedArray = data[0].concat(data[1]);
         //this should probably be changed completely
+        //this code adds site names to the returned object
         for (i = 0; i < mergedArray.length; i++) {
             for (j = 0; j < titles.length; j++) {
-                if (mergedArray[i].siteId == idFields[j].dataset.siteId) {
+                if (mergedArray[i].siteId == idFields[j].value) {
                     mergedArray[i].siteName = titles[j];
                 }                    
             }
@@ -76,7 +77,7 @@ var generateGraphBtn = document.getElementById("generateGraphBtn");
 let apiUrl = generateGraphBtn.dataset.baseapiurl;
 console.log(apiUrl);
 generateGraphBtn.addEventListener("click",
-    create_multi_price_graph.bind(null, apiUrl, "inputSiteId", "Unleaded", "vegagraph1"));
+    create_multi_price_graph.bind(null, apiUrl, "inputSiteName", "inputSiteId", "Unleaded", "vegagraph1"));
 
 //create data source for typeahead
 var sites = new Bloodhound({
@@ -97,11 +98,14 @@ var sites = new Bloodhound({
 });
 
 //create typeahead for text fields
-$('.inputSiteId').typeahead(null, {
+$('.userInputSiteName').typeahead(null, {
     name: 'sites',
     display: 'name',
     source: sites
 }).on('typeahead:selected', function (evt, item) {
-    evt.currentTarget.dataset.siteId = item.id;
-    evt.currentTarget.dataset.siteName = item.name;
+    let fieldNo = evt.currentTarget.dataset.id;
+    let storeIdFields = document.getElementsByClassName("inputSiteId");
+    let storeNameFields = document.getElementsByClassName("inputSiteName");
+    storeIdFields[fieldNo].value = item.id;
+    storeNameFields[fieldNo].value = item.name;
 });
