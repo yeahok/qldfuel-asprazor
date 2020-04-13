2437d784c59f779f617fe6e90f8a0675d052e430
yeahok <yeahok@users.noreply.github.com>
2020-04-13 22:08:45 +1000

    Add display names to sites model

--- qldfuelanalyse/Models/Site.cs
+++ qldfuelanalyse/Models/Site.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,21 @@ namespace qldfuelanalyse.Models
     public class Site
     {
         public int SiteId { get; set; }
+
+        [Display(Name = "Name")]
         public string SiteName { get; set; }
+
+        [Display(Name = "Brand")]
         public string SiteBrand { get; set; }
+
+        [Display(Name = "Address")]
         public string SitesAddressLine1 { get; set; }
+
+        [Display(Name = "Suburb")]
         public string SiteSuburb { get; set; }
         public string SiteState { get; set; }
+
+        [Display(Name = "Postcode")]
         public int? SitePostCode { get; set; }
         public decimal? SiteLatitude { get; set; }
         public decimal? SiteLongitude { get; set; }
