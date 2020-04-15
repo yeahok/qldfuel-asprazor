22ac1ae9d70f35f69fcfe192c55b57a76e824a21
yeahok <yeahok@users.noreply.github.com>
2020-04-15 20:55:54 +1000

    Add empty map to map page

---
+++ qldfuelanalyse/wwwroot/js/mappage.js
@@ -0,0 +1,7 @@
+﻿//set up leaflet map
+var sitelat = -27.4698;
+var sitelong = 153.0251;
+var mymap = L.map('mapid').setView([sitelat, sitelong], 8);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
