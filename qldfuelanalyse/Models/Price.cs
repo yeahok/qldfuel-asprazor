a7d5f8e5da5078a5aa1b6bea87880555a29db396
yeahok <yeahok@users.noreply.github.com>
2022-09-19 22:28:56 +1000

    Set display format of price dates to dd/mm/yyyy

--- qldfuelanalyse/Models/Price.cs
+++ qldfuelanalyse/Models/Price.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace qldfuelanalyse.Models
         public int SiteId { get; set; }
         public string FuelName { get; set; }
         public int Amount { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss}")]
         public DateTime TransactionDate { get; set; }
     }
 }
